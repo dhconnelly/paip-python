526c957db5418f84ce7d6fa62bbbaf1c877b02cc
Daniel Connelly <dhconnelly@gmail.com>
2012-01-19 23:01:57 -0500

    pylint on gps.py

--- gps/gps.py
+++ gps/gps.py
@@ -5,6 +5,27 @@ General Problem Solver: a framework for applying means-ends analysis to solve
 problems expressable as collections of states and operators that induce state
 transitions.
 
+The input file should be a JSON description of the problem domain, containing
+a list of start states, goal states, and operations that move between states.
+Operations are specified by a list of states that must hold before it can be
+applied (preconditions), a list of states to be added after application, and
+a list of states to be deleted after application.
+
+Example:
+
+{
+  "start": ["state1", "state2"],
+  "finish": ["state3"],
+  "ops": [
+    {
+      "action": "foo",
+      "preconds": ["state1", "state2"],
+      "add": ["state3"],
+      "delete": ["state2"]
+    }
+  ]
+}
+
 Translated from chapter 4 of "Paradigms of Artificial Intelligence
 Programming" by Peter Norvig.
 """
@@ -19,15 +40,16 @@ import logging
 
 
 def debug(level, msg):
-    logging.debug("    " * level + msg)
+    """Print msg at the indicated indent level."""
+    logging.debug('    ' * level + msg)
     
 
 def gps(state, goals, ops):
-    """Return a sequence of operations that achieve the specified goal states."""
-    prefix = 'Executing '
-    for op in ops:
-        op['add'].append(prefix + op['action'])
-    return [g for g in achieve_all(state, ops, goals, []) if g.startswith(prefix)]
+    """Return a list of operations that achieve the specified goal states."""
+    pre = 'Executing '
+    for operation in ops:
+        operation['add'].append(pre + operation['action'])
+    return [g for g in achieve_all(state, ops, goals, []) if g.startswith(pre)]
 
 
 def achieve_all(state, ops, goals, goal_stack):
@@ -46,7 +68,7 @@ def achieve(state, ops, goal, goal_stack):
     Returns None if infinite recursion is detected in the goal stack.
     Otherwise, returns a state where goal is satisfied.
     """
-    debug(len(goal_stack), "Goal: %s" % goal)
+    debug(len(goal_stack), 'Goal: %s' % goal)
     
     if goal in state:
         return state
@@ -54,18 +76,18 @@ def achieve(state, ops, goal, goal_stack):
         return None
 
     appropriate = (op for op in ops if is_appropriate(op, goal))
-    for op in appropriate:
-        result = apply_op(op, state, ops, goal, goal_stack)
+    for operation in appropriate:
+        result = apply_operation(operation, state, ops, goal, goal_stack)
         if result:
             return result
 
     
-def is_appropriate(op, goal):
+def is_appropriate(operation, goal):
     """Determines if the operation will add goal to the state."""
-    return goal in op['add']
+    return goal in operation['add']
 
 
-def apply_op(op, state, ops, goal, goal_stack):
+def apply_operation(operation, state, ops, goal, goal_stack):
     """Applies op to the current state if op is applicable.
 
     An operation is applicable when all its preconditions are satisfied;
@@ -73,34 +95,37 @@ def apply_op(op, state, ops, goal, goal_stack):
     precondition cannot be satisfied, returns None.  Otherwise, returns the
     result of applying op to state.
     """
-    debug(len(goal_stack), "Consider: %s" % op['action'])
-    result = achieve_all(state, ops, op['preconds'], [goal] + goal_stack)
+    debug(len(goal_stack), 'Consider: %s' % operation['action'])
+    result = achieve_all(state, ops, operation['preconds'], [goal] + goal_stack)
     if not result:
         return None
-    debug(len(goal_stack), "Action: %s" % op['action'])
-    return [s for s in result if s not in op['delete']] + op['add']
+    debug(len(goal_stack), 'Action: %s' % operation['action'])
+    add, delete = operation['add'], operation['delete']
+    return [s for s in result if s not in delete] + add
 
 
 # ==============================================================================
 # Command line invocation
 
-USAGE = "gps.py [--log=level] problem.json"
+USAGE = 'gps.py [--log=level] problem.json'
 def check_usage(args):
+    """Check the command line arguments."""
     if len(args) < 1:
         print USAGE
         sys.exit(1)
     
 
 def main(args):
+    """Run GPS on the indicated problem file."""
     check_usage(args)
     if args[0].startswith('--log='):
         level = args[0][len('--log='):]
         logging.basicConfig(level=getattr(logging, level.upper(), None))
         args = args[1:]
-            
+
     check_usage(args)
-    with open(args[0]) as f:
-        problem = json.loads(f.read())
+    with open(args[0]) as problem_file:
+        problem = json.loads(problem_file.read())
         start = problem['start']
         finish = problem['finish']
         ops = problem['ops']
